86067c674a76cbdd728ad12595b516b2fa3482a1
7miguelsilva7 <7miguelsilva7@gmail.com>
2017-07-26 14:57:52 -0300

    start

---
+++ gerar-site.sh
@@ -0,0 +1,8 @@
+#gerar site
+
+hugo
+cd docs
+mv index.html menu.html
+cp /opt/lampp/htdocs/playlist/detectar-dispositivo/frame.html .
+cp /opt/lampp/htdocs/playlist/detectar-dispositivo/index.html .
+
